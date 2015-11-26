1197703718db0a2b9473a34596d120ae8511720e
Serhii Kryvokin <serh.kryvokin@gmail.com>
2015-11-26 14:44:32 +0200

    some docs added

--- src/main/java/com/imdb/service/MovieServiceImpl.java
+++ src/main/java/com/imdb/service/MovieServiceImpl.java
@@ -13,6 +13,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Implements API for all required logic related to movies.
+ */
 @Service
 @Transactional
 public class MovieServiceImpl implements MovieService {
@@ -20,22 +23,44 @@ public class MovieServiceImpl implements MovieService {
     private MovieRepository movieRepository;
     private UserService userService;
 
+    /**
+     * Autowiring beans.
+     * @param movieRepository repository bean that is responsible for any db operations related to movies.
+     * @param userService implements API for all required logic related to user.
+     */
     @Autowired
     public MovieServiceImpl(MovieRepository movieRepository, UserService userService) {
         this.movieRepository = movieRepository;
         this.userService = userService;
     }
 
+    /**
+     * See repository docs.
+     * @return list of all movies available in db.
+     * @throws DataAccessException
+     */
     @Override
     public Collection<Movie> findAll() throws DataAccessException {
         return movieRepository.findAll();
     }
 
+    /**
+     * See repository docs.
+     * @param s the string that will be specified for search pattern
+     * @return list of movies matched the pattern.
+     * @throws DataAccessException
+     */
     @Override
     public Collection<Movie> findAll(String s) throws DataAccessException {
         return movieRepository.findAll(s);
     }
 
+    /**
+     *
+     * @param userDetails information about logged user
+     * @return list of movies rated by logged user.
+     * @throws DataAccessException
+     */
     @Override
     public Collection<Movie> findAll(UserDetails userDetails) throws DataAccessException {
         if (userDetails == null)
@@ -44,6 +69,13 @@ public class MovieServiceImpl implements MovieService {
             return movieRepository.findAll(userService.findOneByLogin(userDetails.getUsername()));
     }
 
+    /**
+     *
+     * @param userDetails information about logged user
+     * @param s the string that will be specified for search pattern
+     * @return list of movies rated by logged user and matched the pattern
+     * @throws DataAccessException
+     */
     @Override
     public Collection<Movie> findAll(UserDetails userDetails, String s) throws DataAccessException {
         if (userDetails == null)
@@ -52,36 +84,71 @@ public class MovieServiceImpl implements MovieService {
             return movieRepository.findAll(userService.findOneByLogin(userDetails.getUsername()), s);
     }
 
+    /**
+     *
+     * @param id movie id
+     * @return movie with specified id
+     * @throws DataAccessException
+     */
     @Override
     public Movie findById(Integer id) throws DataAccessException {
         return movieRepository.findById(id);
     }
 
+    /**
+     *
+     * @param movie the movie to save
+     * @throws DataAccessException
+     */
     @Override
     public void save(Movie movie) throws DataAccessException {
         movieRepository.save(movie);
     }
 
+    /**
+     *
+     * @param movie the movie that already exist in db and need to be updated
+     * @throws DataAccessException
+     */
     @Override
     public void update(Movie movie) throws DataAccessException {
         movieRepository.update(movie);
     }
 
+    /**
+     *
+     * @param movie movie to be removed
+     * @throws DataAccessException
+     */
     @Override
     public void remove(Movie movie) throws DataAccessException {
         movieRepository.remove(movie);
     }
 
+    /**
+     *
+     * @param id id of the movie to be removed
+     * @throws DataAccessException
+     */
     @Override
     public void remove(Integer id) throws DataAccessException {
         movieRepository.remove(movieRepository.findById(id));
     }
 
+    /**
+     * Recalculates average rating of all movies
+     * @throws DataAccessException
+     */
     @Override
     public void refreshAverageRating() throws DataAccessException {
         refreshAverageRating(movieRepository.findAll());
     }
 
+    /**
+     * Recalculates average rating of the movie that was recently rated
+     * @param rating new Rating instance, created after performing rate operation by some user
+     * @throws DataAccessException
+     */
     @Override
     public void commitRating(Rating rating) throws DataAccessException {
         Movie movie = rating.getPrimaryKey().getMovie();
@@ -99,12 +166,22 @@ public class MovieServiceImpl implements MovieService {
         movieRepository.update(movie);
     }
 
+    /**
+     * Recalculates average rating of all movies contained in the specified Collection.
+     * @param movies the collection of movies for average rating recalculation
+     * @throws DataAccessException
+     */
     private void refreshAverageRating(Collection<Movie> movies) throws DataAccessException {
         for (Movie movie : movies){
             refreshAverageRating(movie);
         }
     }
 
+    /**
+     * Recalculates average rating of the specified movie
+     * @param movie the movie whose average rating should be recalculated
+     * @throws DataAccessException
+     */
     private void refreshAverageRating(Movie movie) throws DataAccessException {
         int sum = 0;
         double average = 0;
